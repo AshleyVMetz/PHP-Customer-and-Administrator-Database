16401b8e3be5ab0442cb70a196a38e870062a055
Ashley Metz <mrsas@192.168.0.5>
2019-03-13 14:48:31 -0600

    Added my name to the footer

--- view/shared/footer.php
+++ view/shared/footer.php
@@ -1,7 +1,7 @@
 
 <footer>
     <p class="copyright">
-        &copy; <?php echo date("Y"); ?> SportsPro, Inc.
+        &copy; <?php echo date("Y"); ?> Ashley's SportsPro, Inc.
     </p>
 </footer>
 </body>
