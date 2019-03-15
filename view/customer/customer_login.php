067409efcd8f91d70814ae772ae1d720809a1886
Ashley Metz <mrsas@192.168.0.5>
2019-03-15 14:32:48 -0600

    Added logout php and error php

--- view/customer/customer_login.php
+++ view/customer/customer_login.php
@@ -2,13 +2,24 @@
 <main>
 
     <h2>Customer Login</h2>
+    <?php 
+    if(!empty($error)){
+        echo('<div style="color:red;">'.$error.'</div>');
+    }
+    ?>
     <p>You must login before you can register a product.</p>
+     <?php if (isset($logout)) : ?>
+        <p><?php echo $logout; ?></p>
+          <?php endif; ?>  
     <!-- display a search form -->
     <form action="." method="post" id="aligned">
         <input type="hidden" name="action" value="get_customer">
         <label>Email:</label>
         <input type="text" name="email" 
-               value="<?php echo htmlspecialchars($email); ?>">
+               value="<?php echo htmlspecialchars($email); ?>"><br>
+        <label>Password:</label>
+        <input type="text" name="password" 
+               value="<?php echo htmlspecialchars($password); ?>">
         <br>
         <label>&nbsp;</label>
         <input type="submit" value="Login">
